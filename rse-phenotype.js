5b91738b6577d05cddbeed0e61d9e0525d501385
Vanessa Sochat <vsochat@stanford.edu>
2019-07-30 08:58:31 -0400

    adding basic cache for settings
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- rse-phenotype.js
+++ rse-phenotype.js
@@ -1,17 +1,19 @@
 // Ensure the saved image has a white background
 var backgroundColor = 'white';
 Chart.plugins.register({
-    beforeDraw: function(c) {
-        var ctx = c.chart.ctx;
-        ctx.fillStyle = backgroundColor;
-        ctx.fillRect(0, 0, c.chart.width, c.chart.height);
-    }
+  beforeDraw: function(c) {
+    var ctx = c.chart.ctx;
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0, 0, c.chart.width, c.chart.height);
+  }
 });
 
 new Vue ({
   el: '#app',
   data: {
     showShare: false,
+    datasets: null,
+    message: "",
     groups: "open source, academia",
     labels: "Software Engineering, Open Source Development, User Support, Research, Documentation",
     chart: undefined,
@@ -27,9 +29,26 @@ new Vue ({
     }    
   },
   mounted: function() {
+    // Check local storage for cached settings
+    if (localStorage.groups) {
+      this.groups = localStorage.groups;
+    }
+    if (localStorage.labels) {
+      this.labels = localStorage.labels;
+    }
+    if (localStorage.title) {
+      this.title = localStorage.title;
+    }
     this.render();
   },
   methods: {
+
+    persist() {
+      localStorage.labels = this.labels;
+      localStorage.groups = this.groups;
+      localStorage.title = this.title;
+    },
+
     clone: function(obj) {
       return JSON.parse(JSON.stringify(obj));
     },
@@ -107,6 +126,31 @@ new Vue ({
         }
     },
 
+    randomize: function() {
+      this.resetDatasets();
+      this.render();    
+    },
+
+    reset: function() {
+        localStorage.clear();
+        this.groups = "open source, academia",
+        this.labels = "Software Engineering, Open Source Development, User Support, Research, Documentation",
+        this.title = "Research Software Engineer Phenotype: Dinosaur",
+        this.randomize();
+    },
+
+    resetDatasets: function() {
+
+      // Generate datasets if not loaded from cache
+      var datasets = Array();
+      var groups = this.getGroups();
+
+      for(var i = 0; i < groups.length; i++) {
+        datasets.push(this.genData(groups[i]));
+      }
+      this.datasets = datasets;
+
+    },
     render: function() {
       if (this.chart)
         this.chart.destroy()
@@ -116,14 +160,13 @@ new Vue ({
       ctx.fillStyle = "#ffffff";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-      var groups = this.getGroups();
-
-      // Generate datasets
-      var datasets = Array();
-      for(var i = 0; i < groups.length; i++) {
-          datasets.push(this.genData(groups[i]));
+      // Generate datasets if not loaded from cache
+      if (this.datasets == null) {
+        this.resetDatasets()
       }
-
+      datasets = this.datasets
+      
+      // To persist
       this.chart = new Chart(ctx, {
           type: 'radar',
           data: {
@@ -143,6 +186,7 @@ new Vue ({
             }
           }
       });
+      this.persist();
     }
   }
 })
